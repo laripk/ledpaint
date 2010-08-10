0e5e5ff0299a8ca1ec91ad5cae9be2af0814e13a
Lari Kirby <larikirby@fastmail.fm>
2010-08-10 02:51:31 -0400

    oops, this belonged in the previous commit

--- wxruby/appwx.rb
+++ wxruby/appwx.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'wx'
 require 'wx_sugar/all'
 
+require 'mainwindow'
+
 
 class AppWx < Wx::App
   def on_init
@@ -10,24 +12,5 @@ class AppWx < Wx::App
   end
 end
 
-#----- MainWindow -------------------------------------
-class MainWindow < Wx::Frame
-  public
-    def initialize()
-      super(nil, :title => "LED Paint",
-          :style => Wx::DEFAULT_FRAME_STYLE | Wx::TAB_TRAVERSAL,
-          :size => [600,400]
-        )
-        setup_controls
-        setup_events
-    end
-    
-  protected
-    def setup_controls
-    end
-
-    def setup_events
-    end
-end
 
 AppWx.new.main_loop
